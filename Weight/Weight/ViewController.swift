d42e27cdc8adeca5deeafc7047d7e43abb37248d
j-won <j.won@navercorp.com>
2021-03-01 13:20:17 +0900

    chore: Initial XCode 프로젝트

---
+++ Weight/Weight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weight
+//
+//  Created by USER on 2021/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
