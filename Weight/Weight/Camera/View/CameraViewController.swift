da8820b24d0035339f8b53b967f350e74bdc96b5
j-won <j.won@navercorp.com>
2021-03-01 15:17:10 +0900

    feat: Camera UI 구성

--- Weight/Weight/Camera/View/CameraViewController.swift
+++ Weight/Weight/Camera/View/CameraViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker: UIImagePickerController = UIImagePickerController()
+    var isFirstTime: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.mediaTypes = [kUTTypeImage as String]
+        imagePicker.allowsEditing = false
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        if isFirstTime {
+            presentCameraView()
+            isFirstTime = false
+        }
+    }
+    
+    func presentCameraView() {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            present(imagePicker, animated: false, completion: nil)
+        }
+    }
 }
